4352faaa526db122c5d8771b5333117427b6512c
ChesneyW <chesneywilken@gmail.com>
2023-05-01 18:18:05 +0100

    Initial commit of the flash cards react/redux app

---
+++ src/features/topics/topicsSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const topicsSlice = createSlice({
+    name: "topics",
+    initialState: {
+        topics: {}  
+    },
+
+    reducers:{
+        addTopic: (state, action) => {
+            const {id, name, icon} = action.payload;
+            state.topics[id] = {
+                id: id, // this has been destructured. Otherwise it would be id: id.action.payload
+                name: name, //this has been destructured. name: name.action.payload
+                icon: icon,
+                quizId: []
+            }
+        }
+    }
+});
+
+export const {addTopic} = topicsSlice.actions;
+export const selectTopics = state => state.topics.topics;
+export default topicsSlice.reducer;
