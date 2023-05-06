466e0910abe33486da6ccd5867e75cecd96582eb
ChesneyW <chesneywilken@gmail.com>
2023-05-06 12:46:07 +0100

    Started adding the create new quiz feature, there is currently an error in the topicSlice preventing submition of new quiz forms

--- src/features/quizzes/quizzesSlice.js
+++ src/features/quizzes/quizzesSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { addQuizIdForTopic } from "../topics/topicsSlice";
 
 export const quizzesSlice = createSlice({
   name: "quizzes",
@@ -8,13 +9,29 @@ export const quizzesSlice = createSlice({
   reducers: {
     // addQuiz is an action
     addQuiz: (state, action) => {
-      const { id } = action.payload;  /* When payload is received the id property is extracted */
-      state.quizzes[id] = action.payload; /* the payload is a quiz object and this line  */
-    } 
+      /* When payload is received the id property is extracted */
+      const { id } = action.payload;
+      /* the payload is a quiz object and this line  */
+      state.quizzes[id] = action.payload;
+    }
   }
 });
 
-export const { addQuiz } = quizzesSlice.actions; /* destructuring the actions object and exporting only add quiz as a named export. Exported like this to only expose this part of the the actions object to other parts of the application. */
-export const selectQuizzes = state => state.quizzes.quizzes;
-export default quizzesSlice.reducer;
+/* This code exports a function that is wrapped in a thunk.
+Adds a quiz to the 'quizzes' slice and adds that quiz's 'id' to the 'quizIds array of the corresponding topic this the 'topics' slice of the Redux store.
+*/
+export const addQuizForTopicId = (quiz) => {
+  const { topicId, id } = quiz;
 
+  return (dispatch) => {
+    /* This action adds the quiz object to the quizzes slice in the redux store */
+    dispatch(quizzesSlice.actions.addQuiz(quiz));
+    /* This is also an action. This action adds the id property of the quiz object to quizIds array of the topic object in the topics slice in the Redux store */
+    dispatch(addQuizIdForTopic({ topicId: topicId, quizId: id }));
+  };
+};
+
+export const selectQuizzes = (state) => state.quizzes.quizzes;
+ /* destructuring the actions object and exporting only add quiz as a named export. Exported like this to only expose this part of the the actions object to other parts of the application. */
+export const { addQuiz } = quizzesSlice.actions;
+export default quizzesSlice.reducer;
--- src/features/topics/topicsSlice.js
+++ src/features/topics/topicsSlice.js
@@ -5,21 +5,26 @@ export const topicsSlice = createSlice({
     initialState: {
         topics: {}  
     },
-
-    reducers:{
+    reducers: {
         addTopic: (state, action) => {
-            const {id, name, icon} = action.payload;
+            const { id, name, icon } = action.payload;
             state.topics[id] = {
-                id: id, // this has been destructured. Otherwise it would be id: id.action.payload
-                name: name, //this has been destructured. name: name.action.payload
-                icon: icon,
-                quizId: []
-            }
+                id: id,
+                name: name,
+                icon,
+                quizIds: [] // needs to be quizIds
+            };
+        },
+        addQuizIdForTopic: (state, action) => {
+            //extract the required id's from the payload            
+            const { topicId, quizId } = action.payload;
+            /* topicId is used to access the correct topic object in the current state */
+            state.topics[topicId].quizIds.push(quizId);
         }
     }
 });
 
-export const { addTopic } = topicsSlice.actions;
+export const { addTopic, addQuizIdForTopic } = topicsSlice.actions;
 export const selectTopics = (state) => state.topics.topics;
 /* this could also be given directly to the useSelector hook in Topics.js like this const selectTopic = useSelector((state) => state.topics.topics)*/
 export default topicsSlice.reducer;
