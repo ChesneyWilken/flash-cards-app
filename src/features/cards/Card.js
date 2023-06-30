557fd4251e58d6baac6199f05b8d473194fc26e9
ChesneyW <chesneywilken@gmail.com>
2023-06-30 20:44:48 +0100

    Update Card.js
    Removed unused imports

--- src/features/cards/Card.js
+++ src/features/cards/Card.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Link, useParams } from "react-router-dom";
 import { selectCards } from '../cards/cardsSlice';
 import { useSelector } from "react-redux";
 
